f40ba30219149129687c763818188196fbfa6092
hyul77 <kimhyul77@gmail.com>
2024-01-08 20:54:07 +0900

    패턴추가

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const parser = new Parser({
     // 피드 목록
     const feed = await parser.parseURL('https://hyul-code.tistory.com/rss');
 
-    // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 3; i++) {
+    // 최신 2개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 2; i++) {
         const { title, link } = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);

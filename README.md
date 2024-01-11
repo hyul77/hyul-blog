6ebc872191d6aa00a1bf1d9dbeeb47249a606668
hyul77 <kimhyul77@gmail.com>
2024-01-11 09:40:10 +0900

    0111

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-## 2024-01-09
-
-<a href="https://hyul-code.tistory.com/77">Compiler - Organization of Compiler</a></br><a href="https://hyul-code.tistory.com/80">[Django] 첫 설치</a></br>
+<a href="https://hyul-code.tistory.com/80">[Django] 첫 설치</a></br><a href="https://hyul-code.tistory.com/79">(back) Web, Was server</a></br></br>**2024-01-11 업데이트**</br>
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import Parser from "rss-parser";
  */
 let text = '';
 
+// 현재 날짜 생성
+const currentDate = new Date();
+const formattedDate = currentDate.toISOString().split('T')[0];
+
 // rss-parser 생성
 const parser = new Parser({
     headers: {
@@ -18,13 +22,16 @@ const parser = new Parser({
     const feed = await parser.parseURL('https://hyul-code.tistory.com/rss');
 
     // 최신 2개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 2; i++) {
         const { title, link } = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
         text += `<a href="${link}">${title}</a></br>`;
     }
+
+    // 현재 날짜를 추가
+    text += `</br>**${formattedDate} 업데이트**</br>`;
 
     // README.md 파일에 내용 추가
     appendFileSync('README.md', text, 'utf8', (e) => {
